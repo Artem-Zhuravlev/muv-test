b6c30775119a8ce14e4df132db44e7078a831c37
artem <mstrJesm@gmail.com>
2022-10-30 16:01:29 +0200

    fix linters errors

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -4,23 +4,21 @@ const accordionInit = () => {
   const accordionItem = document.querySelectorAll('.accordion-item');
   const ACTIVE_CLASS = 'active';
 
-
   const _accordionToggler = (e) => {
     const target = e.currentTarget;
     const { children } = target.parentNode;
     [...children].forEach((element) => {
-      if (element != target) {
+      if (element !== target) {
         element.classList.remove(ACTIVE_CLASS);
       }
     });
     target.classList.toggle(ACTIVE_CLASS);
-  }
+  };
 
   accordionItem.forEach((item) => {
     item.addEventListener('click', _accordionToggler, false);
     item.addEventListener('keypress', _accordionToggler, false);
   });
 };
-
 
 export default accordionInit;
